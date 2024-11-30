8a92282e9069840f90e02bc916bc500111927708
Yaritza-Mishell <Yaritzapineda09@gmail.com>
2024-11-29 23:04:50 -0500

    tercero commit

---
+++ NxM_ITM_Lógica/MatrixGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MatrixGame
+{
+    public static class MatrixGenerator
+    {
+        public static int[,] GenerateMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+            Random random = new Random();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = random.Next(1, 10);
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
--- NxM_ITM_Lógica/Program.cs
+++ NxM_ITM_Lógica/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace MatrixGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese las dimensiones de la matriz:");
+            Console.Write("Número de filas (N): ");
+            int rows = int.Parse(Console.ReadLine());
+            Console.Write("Número de columnas (M): ");
+            int cols = int.Parse(Console.ReadLine());
+
+            int[,] matrix = MatrixGenerator.GenerateMatrix(rows, cols);
+            GameLogic.PlayGame(matrix, rows, cols);
+        }
+    }
+}
